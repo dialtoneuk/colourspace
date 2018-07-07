17a3fe680dd41ed69cf487b229405f89e2ec0c07
Lewis Lancaster <lewislancastersoftware@gmail.com>
2018-07-07 01:40:20 +0100

    Fixed git ignore

---
+++ assets/js/compiled.js
@@ -0,0 +1,7 @@
+// Automatic script combiner/builder written by Lewis Lancsater
+// Auto combined at 2018-07-07 02:32:00
+// Will recombine at 2018-07-07 04:32:00
+
+// application.js
+// ==================================
+console.log("Application initialized");
