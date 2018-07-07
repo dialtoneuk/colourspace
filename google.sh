323420fc7063bdf5675a0f853563d6757cbe9d4a
Lewis Lancaster <lewislancastersoftware@gmail.com>
2018-07-07 02:15:04 +0100

    Ballache

--- google.sh
+++ google.sh
@@ -2,5 +2,4 @@
 wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.386
 mv cloud_sql_proxy.linux.386 cloud_sql_proxy
 chmod +x cloud_sql_proxy
-
-./cloud_sql_proxy -instances=offmarket-staging:europe-west1:sql-test=tcp:3306 -credential_file=credentials.json
+./cloud_sql_proxy -instances=colourspace-209220:us-central1:colourspace-us=tcp:3306 -credential_file=credentials.json
