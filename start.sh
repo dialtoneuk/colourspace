48407c6eb8908f16a529f23cd89a70870cc983c4
Lewis Lancaster <lewislancastersoftware@gmail.com>
2018-07-07 02:29:46 +0100

    Fixed Proxy after a thousand commits. Sorry everyone. Automated startup works now!

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./cloud_sql_proxy -instances=offmarket-staging:europe-west1:sql-test=tcp:3306 -credential_file=credentials.json
+./cloud_sql_proxy -instances=colourspace-209220:us-central1:colourspace-us=tcp:3306 -credential_file=credentials.json
